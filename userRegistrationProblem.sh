18de51d519ff5e30e0b62ab7f54fbab885738402
rishika7-hub <srishika970@gmail.com>
2020-10-02 21:01:17 +0530

    Added: last name

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -10,3 +10,11 @@ else
 echo "Invalid first name"
 fi
 
+pat="([\\s\\\'-][A-Z][a-z]*)*"
+lastname="Sinha"
+if [[ $lastname =~ $pat ]]
+then
+echo "Valid last name"
+else
+echo "Invalid last name"
+fi
