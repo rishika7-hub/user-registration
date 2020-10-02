68cdea5e7f2d1ff8d3cca75d271c58bd63d1c3e3
rishika7-hub <srishika970@gmail.com>
2020-10-02 20:56:55 +0530

    Added: first name

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "Welcome to User Registration Problem"
 
+pat="([A-Z][a-z]*)*"
+firstname="Rishika"
+if [[ firstname =~ $pat ]]
+then
+echo "Valid first name"
+else
+echo "Invalid first name"
+fi
+
